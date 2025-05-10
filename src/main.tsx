82fe44a37095b94f20014d93b89d7cbd7d961828
Marcelo <marceloakohlhase@gmail.com>
2025-05-10 08:33:26 -0400

    Feat: Estruturando projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import AppRoutes from './routers/index.tsx'
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>,
 )
